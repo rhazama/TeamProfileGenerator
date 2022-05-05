8833285e23e30c3601a4129b513ba6a2e346b34f
rhazama <rhazama01@gmail.com>
2022-05-05 16:54:42 -0700

    added intern to addEmployee

--- index.js
+++ index.js
@@ -73,13 +73,157 @@ const init = () => {
     ])
     .then(data => {
         const {name, id, email, office, extraEmployee} = data;
-        const manager = new Manager(name, email, office);
+        const manager = new Manager(name, id, email, office);
 
-        employees.push(manager);
+        Employees.push(manager);
         return extraEmployee;
     });
 }
 
+const addEmployee = (data) => {
+    if(data === 'Add Engineer') {
+        return inquirer.prompt([
+            {
+                type: `input`,
+                name: `name`,
+                message: `Engineer: (Required)`,
+                validate: nameInput => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter the engineer's name`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `id`,
+                message: `Engineer's id: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter engineer's id`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `email`,
+                message: `Engineer's email: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter the engineer's id`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `github`,
+                message: `Enter engineer's Github: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter engineer's Github username`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `list`,
+                name: `extraEmployee`,
+                message: `Would you like to add another employee?`,
+                choices: ['Add Engineer', 'Add Intern', 'Finish building my team']
+            }
+        ])
+        .then(data => {
+            const { name, id, email, github, extraEmployee } = data;
+            const engineer = new Engineer(name, id, email, github);
+
+            Employees.push(engineer);
+
+            addEmployee(extraEmployee);
+        })
+    } else if (data === 'Add Intern') {
+        return inquirer.prompt([
+            {
+                type: `input`,
+                name: `name`,
+                message: `Intern: (Required)`,
+                validate: nameInput => {
+                    if(nameInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter your intern's name`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `id`,
+                message: `Intern's id: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter intern's id`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `email`,
+                message: `Intern's email: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter the intern's id`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `input`,
+                name: `school`,
+                message: `Enter intern's school: (Required)`,
+                validate: idInput => {
+                    if(idInput) {
+                        return true;
+                    } else {
+                        console.log(`Please enter intern's school name`);
+                        return false;
+                    }
+                }
+            },
+            {
+                type: `list`,
+                name: `extraEmployee`,
+                message: `Would you like to add another employee?`,
+                choices: ['Add Engineer', 'Add Intern', 'Finish building my team']
+            }
+        ])
+        .then(data => {
+            const { name, id, email, school, extraEmployee } = data;
+            const intern = new Intern(name, id, email, school);
+
+            employee.push(intern);
+
+            addEmployee(extraEmployee);
+        })
+    } else {
+        const html = generatePage(Employees);
+        return writeFile(html);
+    }
+}
 
 init()
     .then(data => {
